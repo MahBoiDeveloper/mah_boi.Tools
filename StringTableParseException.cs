0b1fb08d8eeb8c4b2f1cecdf06cd8a5e993a313f
MahBoiDeveloper <mah.boi.developer@gmail.com>
2021-08-15 23:23:50 +0300

    Улучшена реализация класса-исключения при парсинге файлов строковых таблиц

--- StringTableParseException.cs
+++ StringTableParseException.cs
@@ -7,8 +7,98 @@ namespace mah_boi.Tools
 {
     class StringTableParseException : Exception
     {
+        public enum MessageType : int
+        {
+            Info    = 0, // для подсказок пользователю. фича на будушее
+            Warning = 1,
+            Error   = 2
+        }
+
+        private List<string> InfoList;
+        private List<string> WarningList;
+        private List<string> ErrorList;
+
+        public StringTableParseException()
+        {
+            InfoList    = new List<string>();
+            WarningList = new List<string>();
+            ErrorList   = new List<string>();
+        }
         public StringTableParseException(string message) : base(message)
         {
+        }
+
+        public void AddMessage(string msgText, MessageType msgType)
+        {
+            switch (msgType)
+            {
+                case MessageType.Info:
+                    break;
+                case MessageType.Warning:
+                    WarningList.Add(msgText);
+                    break;
+                case MessageType.Error:
+                    ErrorList.Add(msgText);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void DeleteMessage(string msgText, MessageType msgType)
+        {
+            switch (msgType)
+            {
+                case MessageType.Info:
+                    break;
+                case MessageType.Warning:
+                    WarningList.Remove(msgText);
+                    break;
+                case MessageType.Error:
+                    ErrorList.Remove(msgText);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void ThrowExceptions()
+            => throw new StringTableParseException(ToString());
+
+        public string GetExceptions()
+            => ToString();
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            int i = 0;
+
+            if (InfoList.Count > 0)
+                foreach (var str in InfoList)
+                {
+                    i++;
+                    sb.Append($"Информация №{i}: \n\t" + str);
+                }
+
+            i = 0;
+
+            if (WarningList.Count > 0)
+                foreach (var str in WarningList)
+                {
+                    i++;
+                    sb.Append($"Предупреждение №{i}: \n\t" + str);
+                }
+
+            i = 0;
+
+            if (ErrorList.Count > 0)
+                foreach (var str in ErrorList)
+                {
+                    i++;
+                    sb.Append($"Ошибка №{i}: \n\t" + str);
+                }
+
+            return sb.ToString();
         }
     }
 }
