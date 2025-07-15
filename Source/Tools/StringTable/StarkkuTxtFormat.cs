1dab1e5fd1d312339e49026eca0180669d5df916
MahBoiDeveloper <mah.boi.developer@gmail.com>
2025-07-15 15:54:35 +0300

    Add placeholder for Starkku's txt format of string table

---
+++ Source/Tools/StringTable/StarkkuTxtFormat.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mah_boi.Tools.StringTable
+{
+    public class StarkkuTxtFormat : StringTable
+    {
+        public override void Parse()
+        {
+
+        }
+
+        public override bool IsConvertable()
+        {
+            return true;
+        }
+
+        public override bool IsConvertable(List<StringTableString> db)
+        {
+            return true;
+        }
+
+        public override void Save()
+        {
+            
+        }
+
+        public override void Save(string fileName)
+        {
+            
+        }
+    }
+}
