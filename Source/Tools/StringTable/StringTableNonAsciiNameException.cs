33425a6e648a4582f3bad7334d0c7d12e74947de
MahBoiDeveloper <mah.boi.developer@gmail.com>
2025-07-19 11:36:24 +0300

    Add new exception class

---
+++ Source/Tools/StringTable/StringTableNonAsciiNameException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mah_boi.Tools.StringTable;
+
+public class StringTableNonAsciiNameException : Exception
+{
+    public string Message = "Provided string data has non-ascii name.";
+
+    public StringTableNonAsciiNameException() : base()
+    {
+    }
+}
