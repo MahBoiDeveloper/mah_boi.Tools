72916e5d4ad0f44f2cd9192244229b7f97bbc21f
MahBoiDeveloper <mah.boi.developer@gmail.com>
2025-07-20 18:02:31 +0300

    Add `ForEach` extenstion to LINQ pipeline

---
+++ Source/Tools/StringTable/Extensions/IEnumerableExtenstion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mah_boi.Tools.StringTable.Extensions;
+
+public static class IEnumerableExtenstion
+{
+    /// <summary>
+    /// Apply action changes for all elements of collection container.
+    /// </summary>
+    public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, Action<T> action)
+    {
+        foreach (T element in source)
+            action(element);
+
+        return source;
+    }
+}
