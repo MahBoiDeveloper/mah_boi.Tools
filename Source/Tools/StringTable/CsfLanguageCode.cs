17bb5d4affd4a1547de24c8f562b40119b863df0
MahBoiDeveloper <mah.boi.developer@gmail.com>
2025-07-29 22:05:24 +0300

    Extraction of enum `CsfLanguageCodes`

---
+++ Source/Tools/StringTable/CsfLanguageCode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace mah_boi.Tools.StringTable;
+
+public enum CsfLanguageCode : UInt32
+{
+    US                        = 0,
+    UK                        = 1,
+    German                    = 2,
+    French                    = 3,
+    Spanish                   = 4,
+    Italian                   = 5,
+    Japanese                  = 6,
+    Jabberwockie              = 7,
+    Korean                    = 8,
+    Chinese                   = 9,
+    Russian                   = 17,
+    AresPrimaryLoadingFeature = 4294967295
+}
