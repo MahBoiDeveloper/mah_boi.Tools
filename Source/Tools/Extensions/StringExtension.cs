3ebdf8f84e64309ab243af27049803a9972aefef
MahBoiDeveloper <mah.boi.developer@gmail.com>
2025-08-11 15:11:44 +0300

    Add more string class extension method

--- Source/Tools/Extensions/StringExtension.cs
+++ Source/Tools/Extensions/StringExtension.cs
@@ -16,6 +16,10 @@ public static class StringExtension
     /// <summary>
     /// Checks if string has any non ASCII characters.
     /// </summary>
-
     public static bool HasNonASCIIChars(this string str) => Encoding.UTF8.GetByteCount(str) != str.Length;
+
+    /// <summary>
+    /// Indicates whether the specified string is <see href="null" /> or an empty string ("").
+    /// </summary>
+    public static bool IsNullOrEmpty(this string str) => string.IsNullOrEmpty(str);
 }
