894bd094b6f1dc8ad9339d161f76801aba9dd191
MahBoiDeveloper <mah.boi.developer@gmail.com>
2025-08-11 15:11:30 +0300

    Add BinaryReader extension

---
+++ Source/Tools/Extensions/BinaryReaderExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace mah_boi.Tools.Extensions;
+
+public static class BinaryReaderExtension
+{
+    public static char[] ReadChars(this BinaryReader br, UInt32 length)
+    {
+        List<char> ret = new();
+
+        for (UInt32 i = 0; i < length; i++)
+            ret.Add(br.ReadChar());
+
+        return ret.ToArray();
+    }
+}
